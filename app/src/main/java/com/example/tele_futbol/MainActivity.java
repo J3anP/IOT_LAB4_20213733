40caa7b6d312f6f3dfa71e8c86318dda6ddde17b
J3anP <a20213733@pucp.edu.pe>
2024-10-05 01:53:53 -0500

    pantalla principal hecho

--- app/src/main/java/com/example/tele_futbol/MainActivity.java
+++ app/src/main/java/com/example/tele_futbol/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.tele_futbol;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,10 +24,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //Manejo del boton para el internet
+        Button btIngresar = findViewById(R.id.bt_ingresar);
+        btIngresar.setOnClickListener(view -> {
+            if(haveInternet()){
+                Intent intent = new Intent(this, AppActivity.class);
+                startActivity(intent);
+            }else{
+                NoHaveInternet();
+            }
         });
+
+    }
+
+
+
+    public boolean haveInternet(){
+        //Manejo de la conexión de internet
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager != null) {
+            NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
+
+            if (capabilities != null) {
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void NoHaveInternet(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Sin conexión a Internet");
+        builder.setMessage("No tienes conexión a internet. Por favor, activa la conexión para continuar.");
+
+        builder.setPositiveButton("Configuración", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
